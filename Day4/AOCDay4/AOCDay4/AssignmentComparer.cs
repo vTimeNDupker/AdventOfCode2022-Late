6a764ec13d4d35de96c3e3d14da705546d6f0a82
NDupker <n.dupker@vtime.net>
2023-06-02 14:47:47 +0100

    Day4

---
+++ Day4/AOCDay4/AOCDay4/AssignmentComparer.cs
@@ -0,0 +1,47 @@
+﻿namespace AOCDay4;
+
+public class AssignmentComparer
+{
+    public static int ReadFileAndCompareAssignmentsP1(string fileName)
+    {
+        var lines = File.ReadLines(fileName);
+        int totalFullyContainedIDs = 0;
+        foreach (var line in lines)
+        {
+            if (string.IsNullOrWhiteSpace(line))
+                continue;
+            var assignments = line.Split(new[]{',', '-'}, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            int assignmentLeftLower = int.Parse(assignments[0]);
+            int assignmentLeftUpper = int.Parse(assignments[1]);
+            int assignmentRightLower = int.Parse(assignments[2]);
+            int assignmentRightUpper = int.Parse(assignments[3]);
+
+            if ((assignmentLeftLower >= assignmentRightLower && assignmentLeftUpper <= assignmentRightUpper) ||
+                (assignmentRightLower >= assignmentLeftLower && assignmentRightUpper <= assignmentLeftUpper))
+                totalFullyContainedIDs++;
+        }
+
+        return totalFullyContainedIDs;
+    }
+    public static int ReadFileAndCompareAssignmentsP2(string fileName)
+    {
+        var lines = File.ReadLines(fileName);
+        int totalFullyContainedIDs = 0;
+        foreach (var line in lines)
+        {
+            if (string.IsNullOrWhiteSpace(line))
+                continue;
+            var assignments = line.Split(new[]{',', '-'}, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            int assignmentLeftLower = int.Parse(assignments[0]);
+            int assignmentLeftUpper = int.Parse(assignments[1]);
+            int assignmentRightLower = int.Parse(assignments[2]);
+            int assignmentRightUpper = int.Parse(assignments[3]);
+
+            if ((assignmentLeftUpper >= assignmentRightLower && assignmentLeftLower <= assignmentRightUpper) ||
+                (assignmentRightUpper >= assignmentLeftLower && assignmentRightLower <= assignmentLeftUpper))
+                totalFullyContainedIDs++;
+        }
+
+        return totalFullyContainedIDs;
+    }
+}
---
+++ Day4/AOCDay4/AOCDay4/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AOCDay4;
+
+Console.WriteLine("Total score: " + AssignmentComparer.ReadFileAndCompareAssignmentsP1("Input.txt"));
+Console.WriteLine("Total score: " + AssignmentComparer.ReadFileAndCompareAssignmentsP2("Input.txt"));
+Console.ReadKey();
