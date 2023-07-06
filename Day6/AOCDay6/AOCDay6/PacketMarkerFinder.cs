32f58eda99fac76da72872aaa244a5b60eebb5c4
NDupker <n.dupker@vtime.net>
2023-07-06 16:39:38 +0100

    Day6

---
+++ Day6/AOCDay6/AOCDay6/PacketMarkerFinder.cs
@@ -0,0 +1,45 @@
+﻿namespace AOCDay6;
+
+public class PacketMarkerFinder
+{
+    public static int FindPacketMarker(string fileName, int markerLength)
+    {
+        var lines = File.ReadLines(fileName);
+
+        List<char> letters = new List<char>();
+        int letterID = 1;
+        foreach (var line in lines)
+        {
+            foreach (var letter in line)
+            {
+                letters.Add(letter);
+                if (letters.Count > markerLength)
+                {
+                    letters.RemoveAt(0);
+                }
+                if (letters.Count == markerLength)
+                {
+                    bool hasDuplicates = false;
+                    for (int i = 0; i < letters.Count; i++)
+                    {
+                        for (int j = i+1; j < letters.Count; j++)
+                        {
+                            if (letters[i] == letters[j])
+                            {
+                                hasDuplicates = true;
+                                break;
+                            }
+                        }
+                    }
+                    if (!hasDuplicates)
+                    {
+                        return letterID;
+                    }
+                }
+                letterID++;
+            }
+        }
+        
+        return -1;
+    }
+}
---
+++ Day6/AOCDay6/AOCDay6/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AOCDay6;
+
+
+/*Console.WriteLine("Result 0: " + PacketMarkerFinder.FindPacketMarker("txt0.txt", 14));
+Console.WriteLine("Result 1: " + PacketMarkerFinder.FindPacketMarker("txt.txt", 14));
+Console.WriteLine("Result 2: " + PacketMarkerFinder.FindPacketMarker("txt1.txt", 14));
+Console.WriteLine("Result 3: " + PacketMarkerFinder.FindPacketMarker("txt2.txt", 14));
+Console.WriteLine("Result 4: " + PacketMarkerFinder.FindPacketMarker("txt3.txt", 14));*/
+
+Console.WriteLine("Result P1: " + PacketMarkerFinder.FindPacketMarker("Input.txt", 4));
+Console.WriteLine("Result P2: " + PacketMarkerFinder.FindPacketMarker("Input.txt", 14));
+Console.ReadKey();
